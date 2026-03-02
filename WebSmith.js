7f4221f66c7ad851357d54cec16aa71120fafde9
Joel Jolly <128782382+withinJoel@users.noreply.github.com>
2026-03-02 10:48:01 +0530

    Update WebSmith.js

--- WebSmith.js
+++ WebSmith.js
@@ -5,20 +5,11 @@
 const websmithScriptTag = document.currentScript;
 
 // Loader with callback
-function createScript(src, callback) {
+function createScript(src) {
     const script = document.createElement('script');
     script.src = src;
-    if (callback) script.onload = callback;
     document.head.appendChild(script);
 }
 
-// Step 1: Load Head.js
-createScript("https://cdn.jsdelivr.net/gh/withinjoel/WebSmith@main/Head/Head.js", () => {
-    // Step 2: After Head.js, load user's script
-    const userScriptPath = websmithScriptTag.getAttribute("data-websmith");
-    if (userScriptPath) {
-        createScript(userScriptPath);
-    }
-});
-
-createScript("https://cdn.jsdelivr.net/gh/withinjoel/WebSmith@main/Vercel/Vercel.js");
+createScript("https://cdn.jsdelivr.net/gh/withinjoel/WebSmith@main/Head/Head.js"); // Load Head.js
+createScript("https://cdn.jsdelivr.net/gh/withinjoel/WebSmith@main/Vercel/Vercel.js"); // Load Vercel.js
